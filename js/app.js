64ba94d5abe89f2abdcbd90b397bb7157a890bf6
LaTonya Harrison <allone1@users.noreply.github.com>
2016-03-01 15:59:02 -0700

    Add 3 listener functions to tabs to make tab active and to clear content section

--- js/app.js
+++ js/app.js
@@ -2,9 +2,33 @@
 var main = function () {
 	"use strict";
 	
-	console.log("hello world!");
+	$(".tabs a:nth-child(1)").on("click", function () {
+		// make all tabs inactive
+		$(".tabs span").removeClass("active");
 		
-
+		// make the first tab active
+		$(".tabs a:nth-child(1) span").addClass("active");
+		
+		// empty main content so that we can re-create it
+		$("main .content").empty();
+		
+		//return false so we don't follow the link
+		return false;
+	});
+	
+	$(".tabs a:nth-child(2)").on("click", function () {
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(2) span").addClass("active");
+		$("main .content").empty();
+		return false;
+	});
+	
+	$(".tabs a:nth-child(3)").on("click", function () {
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(3) span").addClass("active");
+		$("main .content").empty();
+		return false;
+	});
 };
 	
 $(document).ready(main);

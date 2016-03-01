bf6e210e803f51e8720144a2dedfb208b1b4d99b
LaTonya Harrison <allone1@users.noreply.github.com>
2016-03-01 16:58:51 -0700

    Add refactoring using a loop

--- js/app.js
+++ js/app.js
@@ -2,37 +2,31 @@
 var main = function () {
 	"use strict";
 	
-	var makeTabActive = function (tabNumber) {
-		// construct the selector from the tabNumber
+	var tabNumber; 	
+	
+	// construct selector from tabNumber
 		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
 		
-		// make all tabs inactive
-		$(".tabs span").removeClass("active");
+	for (tabNumber=1; tabNumber <= 3; tabNumber++) {
 		
-		// make the first tab active
-		$(tabSelector).addClass("active");
-		
+		// set up click handler
+		$(tabSelector).on("click", function (event) {
+			// make all tabs inactive
+			$(".tabs span").removeClass("active");
+			
+			// make the first tab active
+			$(event.target).addClass("active");
+			
+			return false;
+	
+		});
+	}
+	
 		// empty main content so that we can re-create it
 		$("main .content").empty();
-		
-	};
-	
-	$(".tabs a:nth-child(1)").on("click", function () {
-		makeTabActive(1);
-		return false;
-	});
-	
-	$(".tabs a:nth-child(2)").on("click", function () {
-		makeTabActive(2);
-		return false;
-	});
-	
-	$(".tabs a:nth-child(3)").on("click", function () {
-		makeTabActive(3);
-		return false;
-	});
-	
+
 };
 	
 $(document).ready(main);
-	
+
+

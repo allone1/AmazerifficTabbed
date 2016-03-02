01d348097c7211c5f413edf3eae851f4ef4513fb
LaTonya Harrison <allone1@users.noreply.github.com>
2016-03-02 12:12:24 -0700

    Add forEach loops to each click handler to dynamically add todos to main .content

--- js/app.js
+++ js/app.js
@@ -2,13 +2,23 @@
 var main = function () {
 	"use strict";
 	
+	var toDos = [
+		"Finish writing book",
+		"Take Gracie to the park",
+		"Answer emails",
+		"Prep for Monday's class",
+		"Make up some new ToDos",
+		"Get Groceries"				
+	];
+	
 	$(".tabs span").toArray().forEach(function (element) {
 			// create a click handler for this element
 			$(element).on("click", function () {
 				// since we're using the JQuery version of element,
 				// we'll go ahead and create a temporary variable
 				// so we don't need to keep recreating it
-				var $element = $(element);
+				var $element = $(element),
+					$content;
 				
 				// make all tabs inactive
 				$(".tabs span").removeClass("active");
@@ -20,16 +30,25 @@ var main = function () {
 				$("main .content").empty();
 				
 				if ($element.parent().is(":nth-child(1)")) {
-					console.log("FIRST TAB CLICKED!");
+					$content = $("<ul>");
+					toDos.forEach(function (toDo) {
+							$content.prepend($("<li>").text(toDo));
+					});
+					$("main .content").append($content);
 				} else if ($element.parent().is(":nth-child(2)")) {
-					console.log("SECOND TAB CLICKED!");
+					$content = $("<ul>");
+					toDos.forEach(function (toDo) {
+							$content.append($("<li>").text(toDo));
+					});
+					$("main .content").append($content);
 				} else if ($element.parent().is(":nth-child(3)")) {
 					console.log("THIRD TAB CLICKED!");
 				}
 				
 				return false;
 	
-			});
+			});			
+			
 	});
 	
 	

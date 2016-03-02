919794f3dbae0ca17faaf93890445d717489fbe1
LaTonya Harrison <allone1@users.noreply.github.com>
2016-03-02 11:08:06 -0700

    Add jq .parent and .is functions to determine which tab has been clicked

--- js/app.js
+++ js/app.js
@@ -3,17 +3,31 @@ var main = function () {
 	"use strict";
 	
 	$(".tabs span").toArray().forEach(function (element) {
+			// create a click handler for this element
 			$(element).on("click", function () {
-			// make all tabs inactive
-			$(".tabs span").removeClass("active");
-			
-			// make the first tab active
-			$(element).addClass("active");
-			
-			// empty main content so that we can re-create it
-			$("main .content").empty();
-			
-			return false;
+				// since we're using the JQuery version of element,
+				// we'll go ahead and create a temporary variable
+				// so we don't need to keep recreating it
+				var $element = $(element);
+				
+				// make all tabs inactive
+				$(".tabs span").removeClass("active");
+				
+				// make the first tab active
+				$(element).addClass("active");
+				
+				// empty main content so that we can re-create it
+				$("main .content").empty();
+				
+				if ($element.parent().is(":nth-child(1)")) {
+					console.log("FIRST TAB CLICKED!");
+				} else if ($element.parent().is(":nth-child(2)")) {
+					console.log("SECOND TAB CLICKED!");
+				} else if ($element.parent().is(":nth-child(3)")) {
+					console.log("THIRD TAB CLICKED!");
+				}
+				
+				return false;
 	
 			});
 	});

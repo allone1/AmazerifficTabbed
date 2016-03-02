62fb634ef93cb6a722a95f0c698c3b9f1e9f44e1
LaTonya Harrison <allone1@users.noreply.github.com>
2016-03-01 17:22:29 -0700

    Add refactoring using a forEach loop

--- js/app.js
+++ js/app.js
@@ -2,29 +2,23 @@
 var main = function () {
 	"use strict";
 	
-	var tabNumber; 	
-	
-	// construct selector from tabNumber
-		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-		
-	for (tabNumber=1; tabNumber <= 3; tabNumber++) {
-		
-		// set up click handler
-		$(tabSelector).on("click", function (event) {
+	$(".tabs span").toArray().forEach(function (element) {
+			$(element).on("click", function () {
 			// make all tabs inactive
 			$(".tabs span").removeClass("active");
 			
 			// make the first tab active
-			$(event.target).addClass("active");
+			$(element).addClass("active");
+			
+			// empty main content so that we can re-create it
+			$("main .content").empty();
 			
 			return false;
 	
-		});
-	}
+			});
+	});
 	
-		// empty main content so that we can re-create it
-		$("main .content").empty();
-
+	
 };
 	
 $(document).ready(main);
